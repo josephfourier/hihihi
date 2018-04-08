0f2652f1d1ebb8c458b389e74441f9ce8c7a1021
liuaiping <2779429237@qq.com>
2018-04-08 08:54:29 +0800

    hh

---
+++ src/views/student/honorary/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="zjy-tabs">
+    <el-tabs type="border-card" v-model="current" @tab-click="tabClick">
+      <el-tab-pane label="我的荣誉称号" name="my">
+        <my-honoraries class="zjy-tab__item" :active="!active"></my-honoraries>
+      </el-tab-pane>
+      <el-tab-pane label="全部荣誉称号" name="all">
+        <all-honoraries class="zjy-tab__item" :active="active"></all-honoraries>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import AllHonoraries from './AllHonoraries'
+import MyHonoraries from './MyHonoraries'
+
+export default {
+  data () {
+    return {
+      current: 'my',
+      active: false // 组件通信
+    }
+  },
+  methods: {
+    tabClick (tab, event) {
+      this.active = !this.active
+    }
+  },
+  components: {
+    AllHonoraries,
+    MyHonoraries
+  }
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
